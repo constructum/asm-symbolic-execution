3dab4927939e86a6c57c27308be7bde557d185b1
Giuseppe Del Castillo <constructum@web.de>
2024-05-03 13:58:02 +0200

    Added '-license' option to CLI.

--- CLI.fs
+++ CLI.fs
@@ -4,7 +4,12 @@ open Common
 
 let usage () =
     write
-        ( [ "Usage: dotnet run [OPTION]..."
+        ( [ 
+            #if DEBUG
+            "Usage: dotnet run [OPTION]..."
+            #else
+            "Usage: asm-symbolic-execution [OPTION]..."
+            #endif
             ""
             "Options:"
             "  -str <strg>   load definitions specified in string <strg>"
@@ -20,7 +25,53 @@ let usage () =
             ""
             "  -nosmt        do not use SMT solver"
             ""
+            "  -license      display license information"
             "" ] |> String.concat "\n" )
+
+let license () =
+    write
+        ( [ ""
+            "MIT License"
+            ""
+            "Copyright (c) 2024 Giuseppe Del Castillo"
+            ""
+            "Permission is hereby granted, free of charge, to any person obtaining a copy"
+            "of this software and associated documentation files (the \"Software\"), to deal"
+            "in the Software without restriction, including without limitation the rights"
+            "to use, copy, modify, merge, publish, distribute, sublicense, and/or sell"
+            "copies of the Software, and to permit persons to whom the Software is"
+            "furnished to do so, subject to the following conditions:"
+            ""
+            "The above copyright notice and this permission notice shall be included in all"
+            "copies or substantial portions of the Software."
+            ""
+            "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR"
+            "IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,"
+            "FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE"
+            "AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER"
+            "LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,"
+            "OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE"
+            "SOFTWARE."
+            ""
+            "--------------------------------------------------------------------"
+            ""
+            "THIRD-PARTY LIBRARIES"
+            ""
+            "-----"
+            ""
+            "Z3"
+            "Copyright (c) Microsoft Corporation"
+            "All rights reserved. "
+            "MIT License"
+            ""
+            "Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the \"Software\"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:"
+            ""
+            "The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software."
+            ""
+            "THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE."
+            ""
+            "" ]  |> String.concat "\n" )
+
 
 let exec_symbolic symb_exec_fct =
     let R_in = try AST.get_rule "Main" (!TopLevel.rules) with _ -> failwith "rule 'Main' not defined"
@@ -62,13 +113,14 @@ let CLI_with_ex(args) =
                 else writeln_err $"parameter missing after {option_str}"
             if i < n then 
                 match args[i] with
+                |   "-license"         -> license (); exit 0
                 |   "-symbolic"        -> symbolic := true;    parse_arguments (i+1)
                 |   "-nonsymbolic"     -> symbolic := false;   parse_arguments (i+1)
                 |   "-turbo2basic"     -> turbo2basic := true; parse_arguments (i+1)
                 |   "-nosmt"           -> SymbEval.use_smt_solver := false; parse_arguments (i+1)
                 |   "-str"  -> load "-str"  TopLevel.loadstr  (args[i+1]); parse_arguments (i+2)
                 |   "-file" -> load "-file" TopLevel.loadfile (args[i+1]); parse_arguments (i+2)
-                |   s -> writeln_err $"unknown parameter: {s}"
+                |   s -> writeln_err $"unknown option: {s}"; exit 1
         parse_arguments 0
         if !turbo2basic
         then exec_symbolic SymbEval.symbolic_execution_for_turbo_asm_to_basic_asm_transformation
