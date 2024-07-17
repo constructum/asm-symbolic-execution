342f7f249894c973764c93fa0a3cee8bcb0f8e0f
Giuseppe Del Castillo <constructum@web.de>
2024-07-17 20:16:43 +0200

    Fix indentation in AsmetaL examples.

--- examples-asmeta/bubblesort/bubblesort.asm
+++ examples-asmeta/bubblesort/bubblesort.asm
@@ -4,50 +4,50 @@ import ../STDL/StandardLibrary.asm
 
 
 signature:
-	static n : Integer
-	controlled ii : Integer
-	controlled j : Integer
-	controlled a : Integer -> Integer
-	controlled sorted : Boolean
+    static n : Integer
+    controlled ii : Integer
+    controlled j : Integer
+    controlled a : Integer -> Integer
+    controlled sorted : Boolean
 
 
 definitions:
-	function n = 3
+    function n = 3
 
-	main rule r_Main = seq
-		//--- sorting algorithm
-    	ii := 0
-    	while (ii < n) do seq
-        	j := n - 1
-        	while (j > ii) do seq
-            	if a(j-1) > a(j)
-            	then par a(j-1):=a(j) a(j):=a(j-1) endpar 
-            	endif
-            	j := j - 1
+    main rule r_Main = seq
+        //--- sorting algorithm
+        ii := 0
+        while (ii < n) do seq
+            j := n - 1
+            while (j > ii) do seq
+                if a(j-1) > a(j)
+                then par a(j-1):=a(j) a(j):=a(j-1) endpar 
+                endif
+                j := j - 1
             endseq
-        	ii := ii + 1
+            ii := ii + 1
         endseq
-		//--- verification that the array is sorted
-	    par
-	    	sorted := true
-	    	ii := 0
-	    endpar
-	    while (ii < n - 1) do par
-	    	sorted := sorted and (a(ii) <= a (ii+1))
-	    	ii := ii + 1
-	    endpar
-	endseq
+        //--- verification that the array is sorted
+        par
+            sorted := true
+            ii := 0
+        endpar
+        while (ii < n - 1) do par
+            sorted := sorted and (a(ii) <= a (ii+1))
+            ii := ii + 1
+        endpar
+    endseq
 
 /*
 // some initialization for the simulator, to be commented out for symbolic execution
 
 default init s0:
-	function a($i in Integer) =
-		switch $i
-			case 0 : 5
-			case 1 : 3
-			case 2 : 2
-			case 3 : 7
-			case 4 : 4
-		endswitch
+    function a($i in Integer) =
+        switch $i
+            case 0 : 5
+            case 1 : 3
+            case 2 : 2
+            case 3 : 7
+            case 4 : 4
+        endswitch
 */
